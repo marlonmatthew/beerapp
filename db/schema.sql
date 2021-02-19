fab42424776cf492010c123b8ada5feeb4e35b30
Carl <cfuer12@yahoo.com>
2021-02-18 22:15:21 -0800

    added database

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE beers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(250) NOT NULL,
+    brewery varchar(250) not null,
 	abv decimal (1, 2),
     flavor_notes varchar(250) not null,
     mood varchar (250) not null
