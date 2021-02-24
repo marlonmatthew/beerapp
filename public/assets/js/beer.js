a86a1dd03d9f15360416ba12efdc8ef54f3e28e2
Edwin Troya <edtroyajr@gmail.com>
2021-02-23 18:44:38 -0800

    updating

--- public/assets/js/beer.js
+++ public/assets/js/beer.js
@@ -2,7 +2,7 @@ document.getElementById("age").addEventListener("click", myFunction);
 
 function myFunction() {
   //   const yearInput = document.getElementById("year").value;
-  // if(yearInput > today - 21 years ){
+  if(yearInput > today - 21 years ){
   window.location.href = "/index";
   // }else{
   //error your old enough
