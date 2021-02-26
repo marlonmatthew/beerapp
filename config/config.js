05da66dc9f9d6ce06990c59e3ad9a83b6e9d16dc
Carl <cfuer12@yahoo.com>
2021-02-25 20:51:54 -0800

    added .env file

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.DBPASSWORD,
+    database: "passport_demo",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
