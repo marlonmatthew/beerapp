b9bb854276bd8d8628c7d154ad812cb7f4154504
Edwin Troya <edtroyajr@gmail.com>
2021-02-27 11:20:03 -0800

    removing files that are not needed

--- public/assets/js/dashboard 2.js
+++
@@ -1,9 +0,0 @@
-const logoutButton = document.getElementById("logout-button");
-
-logoutButton.addEventListener("click", () => {
-  fetch("/api/logout", {
-    method: "POST"
-  }).then(() => {
-    window.location.href = "/login";
-  });
-});
