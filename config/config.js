7cb5e004a328312a70e5d14571d76050c3fa78f2
Eric Hunter <ehunter7@live.com>
2021-03-02 09:30:01 -0800

    random beer logic

--- config/config.js
+++ config/config.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 
 module.exports = {
+  // development: {
+  //   username: "tw5rbgt459c0wk0r",
+  //   password: process.env.DBPASSWORD,
+  //   database: "pj4qq2yjmuzx0dqp",
+  //   host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  //   dialect: "mysql"
+  // },
   development: {
-    username: "tw5rbgt459c0wk0r",
+    username: "root",
     password: process.env.DBPASSWORD,
-    database: "pj4qq2yjmuzx0dqp",
-    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    database: "beers",
+    host: "localhost",
     dialect: "mysql"
   },
   test: {
