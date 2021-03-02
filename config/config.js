208d57a986fca4a2d8ed8c90d96725f2f686c878
Eric Hunter <ehunter7@live.com>
2021-03-02 09:31:07 -0800

    uncommenting sonfig.js

--- config/config.js
+++ config/config.js
@@ -1,18 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  // development: {
-  //   username: "tw5rbgt459c0wk0r",
-  //   password: process.env.DBPASSWORD,
-  //   database: "pj4qq2yjmuzx0dqp",
-  //   host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  //   dialect: "mysql"
-  // },
   development: {
-    username: "root",
+    username: "tw5rbgt459c0wk0r",
     password: process.env.DBPASSWORD,
-    database: "beers",
-    host: "localhost",
+    database: "pj4qq2yjmuzx0dqp",
+    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql"
   },
   test: {
