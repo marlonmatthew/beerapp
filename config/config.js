820b2d7024c0cc1efb4f315e79babcf748ed9ee2
Eric Hunter <ehunter7@live.com>
2021-03-01 21:07:22 -0800

    merging main into eric

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "tw5rbgt459c0wk0r",
+    username: "root",
     password: process.env.DBPASSWORD,
-    database: "pj4qq2yjmuzx0dqp",
-    host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    database: "beers",
+    host: "localhost",
     dialect: "mysql"
   },
   test: {
